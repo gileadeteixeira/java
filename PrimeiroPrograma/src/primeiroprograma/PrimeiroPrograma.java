521d7aaeb3450d6bc71316e3b2a993096a26092b
Gileade Aquino Teixeira <gileadeateixeira@gmail.com>
2021-03-17 08:51:01 -0300

    commit

---
+++ PrimeiroPrograma/src/primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,12 @@
+package primeiroprograma;
+import java.io.*;
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) throws java.io.IOException{
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String aux;
+        aux=in.readLine();
+        System.out.println(aux);
+        
+    }
+}
